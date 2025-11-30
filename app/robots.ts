2f692cba74cba4fad0b5f2613469bf296ae0bde5
RanitDERIA <ranitderiaofficial@gmail.com>
2025-11-30 12:02:20 +0530

    Fix sitemap configuration

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/', // Block private routes if you have any
+      disallow: '/private/',
     },
     sitemap: 'https://chefini.vercel.app/sitemap.xml',
   };
