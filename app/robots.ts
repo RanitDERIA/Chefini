2fd1f122851d2328ebbbc527332029ee6248a522
RanitDERIA <ranitderiaofficial@gmail.com>
2025-11-30 12:06:34 +0530

    Fix robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,17 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://chefini.vercel.app';
+  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://chefini.vercel.app/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/private/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
